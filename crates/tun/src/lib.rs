65f6880c05c6c24fb1f2333cbe590f9673b2cde4
m1m1sha <18262227804@163.com>
2024-03-04 16:23:10 +0800

    📃 docs: 参考信息

--- crates/tun/src/lib.rs
+++ crates/tun/src/lib.rs
@@ -1,3 +1,8 @@
+// 参考
+// https://github.com/meh/rust-tun
+// https://github.com/Tazdevil971/tap-windows
+// https://github.com/nulldotblack/wintun
+// https://github.com/lbl8603/vnt
 pub mod device;
 #[cfg(target_os = "windows")]
 pub mod windows;
