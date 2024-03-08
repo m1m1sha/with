1f0a96a21dc8f5011b38befbebf1416a25a29bb1
m1m1sha <18262227804@163.com>
2024-03-08 15:31:01 +0800

    ✨ feat: handler

---
+++ src/handler/handshaker.rs
@@ -0,0 +1,66 @@
+use std::io::Result;
+
+use prost::Message;
+
+use crate::handler::{GATEWAY_IP, SELF_IP};
+use crate::proto::message::HandshakeRequest;
+use crate::proto::message::SecretHandshakeRequest;
+use cipher::RsaCipher;
+use protocol::body::RSA_ENCRYPTION_RESERVED;
+use protocol::{service, NetPacket, Protocol, Version, MAX_TTL};
+
+pub enum HandshakeEnum {
+    NotSecret,
+    KeyError,
+    Timeout,
+    ServerError(String),
+    Other(String),
+}
+
+/// 第一次握手数据
+pub fn handshake_request_packet(secret: bool) -> Result<NetPacket<Vec<u8>>> {
+    let mut request = HandshakeRequest::default();
+    request.secret = secret;
+    request.version = crate::WITH_VERSION.to_string();
+    let binding = request.encode_to_vec();
+    let bytes = binding.as_slice();
+
+    let buf = vec![0u8; 12 + bytes.len()];
+    let mut net_packet = NetPacket::new(buf)?;
+    net_packet.set_version(Version::V1);
+    net_packet.set_gateway_flag(true);
+    net_packet.set_destination(GATEWAY_IP);
+    net_packet.set_source(SELF_IP);
+    net_packet.set_protocol(Protocol::Service);
+    net_packet.set_transport_protocol(service::Protocol::HandshakeRequest.into());
+    net_packet.first_set_ttl(MAX_TTL);
+    net_packet.set_payload(bytes)?;
+    Ok(net_packet)
+}
+
+/// 第二次加密握手
+pub fn secret_handshake_request_packet(
+    rsa_cipher: &RsaCipher,
+    token: String,
+    key: &[u8],
+) -> Result<NetPacket<Vec<u8>>> {
+    let mut request = SecretHandshakeRequest::default();
+    request.token = token;
+    request.key = key.to_vec();
+    let binding = request.encode_to_vec();
+    let bytes = binding.as_slice();
+
+    let mut net_packet = NetPacket::new0(
+        12 + bytes.len(),
+        vec![0u8; 12 + bytes.len() + RSA_ENCRYPTION_RESERVED],
+    )?;
+    net_packet.set_version(Version::V1);
+    net_packet.set_gateway_flag(true);
+    net_packet.set_destination(GATEWAY_IP);
+    net_packet.set_source(SELF_IP);
+    net_packet.set_protocol(Protocol::Service);
+    net_packet.set_transport_protocol(service::Protocol::SecretHandshakeRequest.into());
+    net_packet.first_set_ttl(MAX_TTL);
+    net_packet.set_payload(&bytes)?;
+    Ok(rsa_cipher.encrypt(&mut net_packet)?)
+}
---
+++ src/handler/register.rs
@@ -0,0 +1,46 @@
+use crate::handler::{GATEWAY_IP, SELF_IP};
+use crate::proto::message::RegistrationRequest;
+use cipher::Cipher;
+use prost::Message;
+use protocol::body::ENCRYPTION_RESERVED;
+use protocol::{service, NetPacket, Protocol, Version, MAX_TTL};
+use std::{io::Result, net::Ipv4Addr};
+
+/// 注册数据
+pub fn registration_request_packet(
+    server_cipher: &Cipher,
+    token: String,
+    device_id: String,
+    name: String,
+    ip: Option<Ipv4Addr>,
+    is_fast: bool,
+    allow_ip_change: bool,
+    client_secret: bool,
+) -> Result<NetPacket<Vec<u8>>> {
+    let mut request = RegistrationRequest::default();
+    request.token = token;
+    request.device_id = device_id;
+    request.name = name;
+    if let Some(ip) = ip {
+        request.virtual_ip = ip.into();
+    }
+    request.allow_ip_change = allow_ip_change;
+    request.is_fast = is_fast;
+    request.version = crate::WITH_VERSION.to_string();
+    request.client_secret = client_secret;
+    let binding = request.encode_to_vec();
+    let bytes = binding.as_slice();
+
+    let buf = vec![0u8; 12 + bytes.len() + ENCRYPTION_RESERVED];
+    let mut net_packet = NetPacket::new_encrypt(buf)?;
+    net_packet.set_destination(GATEWAY_IP);
+    net_packet.set_source(SELF_IP);
+    net_packet.set_version(Version::V1);
+    net_packet.set_gateway_flag(true);
+    net_packet.set_protocol(Protocol::Service);
+    net_packet.set_transport_protocol(service::Protocol::RegistrationRequest.into());
+    net_packet.first_set_ttl(MAX_TTL);
+    net_packet.set_payload(&bytes)?;
+    server_cipher.encrypt_ipv4(&mut net_packet)?;
+    Ok(net_packet)
+}
