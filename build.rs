1ae527a1197ea38cecc9cf34665807c244abaaf5
m1m1sha <18262227804@163.com>
2024-03-08 12:33:38 +0800

    ✨ feat: proto

--- build.rs
+++ build.rs
@@ -1,5 +1,3 @@
-use std::io::Result;
-fn main() -> Result<()> {
-    prost_build::compile_protos(&["src/proto/message.proto"], &["src/proto"])?;
-    Ok(())
+fn main() {
+    prost_build::compile_protos(&["proto/message.proto"], &["proto/"]).unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
 pub mod config;
 pub mod external;
+pub mod handler;
 pub mod mode;
+pub mod proto;
+
+pub const WITH_VERSION: &'static str = env!("CARGO_PKG_VERSION");
